4eeb1f6cb164971d2258ca9adddf44fab55fa0e5
Ashim <panghalashim7832@gmail.com>
2025-07-04 20:56:57 +0530

    ThreadS

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,7 +1,11 @@
+import { ClerkProvider, UserButton } from '@clerk/nextjs';
+
 export default function Home() {
   return (
-    <main>
-      <h1>Threads</h1>
-    </main>
+    <div>
+      <ClerkProvider>
+        <UserButton afterSignOutUrl="/" />
+      </ClerkProvider>
+    </div>
   );
 }
