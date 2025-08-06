6534fa55d86b3ec895977f6447d2fdf0f0201bd6
AshimChoudhary <panghalashim7832@gmail.com>
2025-08-06 19:13:49 +0530

    Moving on TO Communities

--- app/(root)/create-thread/page.tsx
+++ app/(root)/create-thread/page.tsx
@@ -1,12 +1,14 @@
+import { currentUser } from '@clerk/nextjs/server';
 import { redirect } from 'next/navigation';
+
 import PostThread from '@/components/forms/PostThread';
 import { fetchUser } from '@/lib/actions/user.actions';
-import { currentUser } from '@clerk/nextjs/server';
 
 async function Page() {
   const user = await currentUser();
   if (!user) return null;
 
+  // fetch organization list created by user
   const userInfo = await fetchUser(user.id);
   if (!userInfo?.onboarded) redirect('/onboarding');
 
@@ -14,7 +16,7 @@ async function Page() {
     <>
       <h1 className="head-text">Create Thread</h1>
 
-      <PostThread userId={userInfo._id} />
+      <PostThread userId={userInfo._id.toString()} />
     </>
   );
 }
